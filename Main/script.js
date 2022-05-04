a05c357c3d44b423aef428fbd711c6301c9bee31
Muhammad Taufiqurrahman <mhmtaufiqurrahman@gmail.com>
2022-05-04 17:20:15 +0700

    Cuman menambahkan tulisan selamat

--- Main/script.js
+++ Main/script.js
@@ -1,10 +1,13 @@
 // Time
 var time = document.querySelector('.time');
+
 const date = new Date();
 
 time.innerHTML = date.getHours() + ':' + date.getMinutes();
 
-// Background
+// Background and time
+var good = document.querySelector('.good');
+
 const bgTime = date.getHours();
 
 function responsiveBody() {
@@ -16,11 +19,18 @@ function responsiveBody() {
 
 if(bgTime < 7 || bgTime >= 19) {
     document.body.style.backgroundImage = 'url(img/malam.jpg)';
+    good.innerHTML = 'Selamat malam, ';
     responsiveBody();
 }else if(bgTime <= 15) {
     document.body.style.backgroundImage = 'url(img/siang.jpg)';
+    if(bgTime <= 10) {
+        good.innerHTML = 'Selamat pagi, ';
+    } else if (bgTime > 10){
+        good.innerHTML = 'Selamat siang, ';
+    }
     responsiveBody();
 }else if(bgTime >= 16) {
     document.body.style.backgroundImage = 'url(img/sore.jpg)';
+    good.innerHTML = 'Selamat sore, ';
     responsiveBody();
 }
